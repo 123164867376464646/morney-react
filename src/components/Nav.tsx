0c3fbc8df09001bda9a08733538424c06f0bd78e
linmumu <1054742101@qq.com>
2022-09-20 04:03:52 +0800

    创建 Icon 组件

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
+import Icon from './Icon';
+
+
 
 const NavWrapper = styled.nav`
   line-height: 24px;
@@ -12,26 +15,38 @@ const NavWrapper = styled.nav`
     > li {
       width: 33.33333%;
       text-align: center;
-      padding: 16px;
+      display: flex;
+      padding: 4px 0;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+
+      > .icon {
+        width: 24px;
+        height: 24px;
+      }
     }
   }
 
 `;
-const Nav= ()=>{
-	return(
+const Nav = () => {
+	return (
 		<NavWrapper>
 			<ul>
 				<li>
-					<Link to="/tags">标签页</Link>
+					<Icon name="label"/>
+					<Link to="/tags">标签</Link>
 				</li>
 				<li>
-					<Link to="/">记账页</Link>
+					<Icon name="money"/>
+					<Link to="/money">记一笔</Link>
 				</li>
 				<li>
-					<Link to="/statistics">统计页</Link>
+					<Icon name="statistics"/>
+					<Link to="/statistics">统计</Link>
 				</li>
 			</ul>
 		</NavWrapper>
-	)
-}
-export default Nav
+	);
+};
+export default Nav;
