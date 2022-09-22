d1f7864f8772395b6d6f655e8dedd017b09a8a44
linmumu <1054742101@qq.com>
2022-09-22 13:30:51 +0800

    抽离 useTags.tsx

--- src/views/Label.tsx
+++ src/views/Label.tsx
@@ -1,11 +1,14 @@
 import Layout from '../components/Layout';
 import React from 'react';
+import {useTags} from '../useTags';
 
 function Tags() {
+	const {tags, setTags} = useTags();
 	return (
 		<Layout>
 			<h1>标签页面</h1>
 		</Layout>
 	);
 }
-export default Tags
+
+export default Tags;
