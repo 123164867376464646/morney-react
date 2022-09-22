f789faa4dea9fe286af72c71ebb649cd0aeb813a
linmumu <1054742101@qq.com>
2022-09-22 14:10:26 +0800

    完成 tag list 样式

--- src/views/Label.tsx
+++ src/views/Label.tsx
@@ -1,12 +1,58 @@
 import Layout from '../components/Layout';
 import React from 'react';
 import {useTags} from '../useTags';
+import styled from 'styled-components';
+import Icon from '../components/Icon';
+
+const TagList = styled.ol`
+  font-size: 16px;
+  background: white;
+
+  > li {
+    border-bottom: 1px solid #d5d5d9;
+    padding: 12px 16px 12px 0;
+    margin-left: 16px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+const Button = styled.button`
+  font-size: 18px;
+  border: none;
+  padding: 8px 12px;
+  border-radius: 4px;
+  background: rgba(241, 95, 20);
+  color: white;
+`;
+const Center = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const Space = styled.div`
+  height: 16px;
+`;
 
 function Tags() {
 	const {tags, setTags} = useTags();
 	return (
 		<Layout>
-			<h1>标签页面</h1>
+			<TagList>
+				{tags.map(t =>
+					<li key={t}>
+						<span className="oneLine">{t}</span>
+						<Icon name="right"/>
+					</li>
+				)}
+			</TagList>
+			<Center>
+				<Space/>
+				<Space/>
+				<Space/>
+				<Button>新增标签</Button>
+			</Center>
 		</Layout>
 	);
 }
