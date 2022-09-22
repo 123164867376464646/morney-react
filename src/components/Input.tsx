7d41e5c14bd2da75cb039b3cd61518cad721822f
linmumu <1054742101@qq.com>
2022-09-22 20:09:39 +0800

    创建单独的 Input 组件

---
+++ src/components/Input.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Label = styled.label`
+
+  display: flex;
+  align-items: center;
+
+  > span {
+    margin-right: 16px;
+    white-space: nowrap;
+  }
+
+  > input {
+    width: 100%;
+    background: none;
+    border: none;
+    height: 72px;
+  }
+`;
+type Props = {
+	label:string,
+} & React.InputHTMLAttributes<HTMLInputElement>
+
+const Input: React.FC<Props> = (props) => {
+	const {label ,children, ...rest} = props
+	return (
+		<Label>
+			<span>{props.label}</span>
+			<input {...rest}/>
+		</Label>
+	);
+};
+export {Input}
