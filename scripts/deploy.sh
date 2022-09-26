73ecec164daa07909d975fc9669ebfd18506fe39
linmumu <1054742101@qq.com>
2022-09-26 14:40:05 +0800

    update title

---
+++ scripts/deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+yarn build &&
+cd build/ &&
+git init &&
+git add . &&
+git commit -m 'deploy' &&
+git remote add origin git@github.com:123164867376464646/morney-website-react.git. &&
+git push -u origin master -f
+cd -
